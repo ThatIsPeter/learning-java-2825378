54ed8552f4e61b8ee8c2910b65eda8e36a049ee3
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 21:08:15 +1300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
+import java.util.Scanner;
+
+import jdk.internal.util.xml.impl.Input;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "what is 1+1";
+        String choiceOne = "1";
+        String choiceTwo = "2";
+        String choiceThree = "3";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
+        System.out.println("2,3,4?");
 
         // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
         // Retrieve the user's input
+        if (correctAnswer.equals(input)) {
+            System.out.println("you did it");
+        } else {
+            System.out.println("I am ashamed, the correct answer was " + correctAnswer);
+        }
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
